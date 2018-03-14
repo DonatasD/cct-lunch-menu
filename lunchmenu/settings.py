b7a07946652a098e75110f19529f33b97cd3cbae
DonatasD <donatas@zenitech.co.uk>
2018-03-14 22:38:03 +0200

    Enable simple file logging

--- lunchmenu/settings.py
+++ lunchmenu/settings.py
@@ -120,3 +120,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + '/cct-lunch-menu.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
