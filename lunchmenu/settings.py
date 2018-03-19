44e20991c1fe081ddf4ce40d6c61181403803057
DonatasD <donatasdaubar@gmail.com>
2018-03-19 17:17:52 +0200

    changed django logging to info

--- lunchmenu/settings.py
+++ lunchmenu/settings.py
@@ -136,7 +136,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'ERROR',
+            'level': 'INFO',
             'propagate': True,
         },
         'api': {
